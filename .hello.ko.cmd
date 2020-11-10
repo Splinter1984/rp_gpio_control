32e095fbe18027ad1872788059348e466b3294d9
Andrianov Roma <47949756+Splinter1984@users.noreply.github.com>
2020-11-10 22:50:40 +0700

    Delete .hello.ko.cmd

--- .hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/hello_world/hello.ko := ld -r  -EL  --build-id  -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds -o /home/pi/hello_world/hello.ko /home/pi/hello_world/hello.o /home/pi/hello_world/hello.mod.o;  true
